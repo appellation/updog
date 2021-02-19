27a6584dbfb793fbb9c1ee9cdb21fb3746c13b01
Will Nelson <will@wnelson.xyz>
2021-02-18 21:44:09 -0800

    Expose API & WS URLs to browser

--- site/src/constants.ts
+++ site/src/constants.ts
@@ -1,2 +1,2 @@
-export const WS_API_BASE = process.env.WS_API_BASE;
-export const HTTP_API_BASE = process.env.HTTP_API_BASE;
+export const WS_API_BASE = process.env.NEXT_PUBLIC_WS_API_BASE;
+export const HTTP_API_BASE = process.env.NEXT_PUBLIC_HTTP_API_BASE;
