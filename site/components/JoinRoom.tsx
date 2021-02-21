832c20c716827406c74c0fe1e8c7176de9a8c862
Will Nelson <will@wnelson.xyz>
2021-02-20 20:27:35 -0800

    Improve UX on join room dialog

--- site/components/JoinRoom.tsx
+++ site/components/JoinRoom.tsx
@@ -3,6 +3,7 @@ import { useRef, RefObject, useState, FormEvent } from 'react'
 import { mutate } from 'swr';
 
 import fetch from '../src/fetch';
+import ErrorSnackbar from './ErrorSnackbar';
 import CenterCard from './ui/CenterCard';
 import Button from './ui/forms/Button';
 import Input from './ui/forms/Input';
@@ -32,14 +33,16 @@ export default function JoinRoom() {
 	const [error, setError] = useState<string>();
 
 	return (
-		<CenterCard>
-			{error && <div>{error}</div>}
-			<h1 className="text-3xl font-bold mb-6">Join Room</h1>
-			<form onSubmit={event => joinRoom(event, roomId, password, setError)}>
-				<label htmlFor="password">Password</label>
-				<Input type="password" id="password" placeholder="Password" ref={password} />
-				<Button primary type="submit">Join</Button>
-			</form>
-		</CenterCard>
+		<>
+			<CenterCard>
+				<h1 className="text-3xl font-bold mb-6">join room</h1>
+				<form onSubmit={event => joinRoom(event, roomId, password, setError)}>
+					<label htmlFor="password" className="sr-only">Password</label>
+					<Input type="password" id="password" placeholder="password" ref={password} />
+					<Button primary type="submit">join</Button>
+				</form>
+			</CenterCard>
+			<ErrorSnackbar message={error} />
+		</>
 	)
 }
